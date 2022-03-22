9a12b6415e52186d645ee617c2047ceaa9131749
Matheus Alves <matheusalves.bmd@gmail.com>
2022-03-21 23:42:21 -0300

    Create the MenuButton component

---
+++ app/components/MenuButton/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MenuButton = () => {
+  return (
+    <button className="sidebar-toggle">
+      <i className="fas fa-bars"></i>
+    </button>
+  );
+};
+
+export default MenuButton;
