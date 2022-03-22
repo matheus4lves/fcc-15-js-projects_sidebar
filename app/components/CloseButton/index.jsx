cf0530260bef8877a5c95f33236308c207b11b0c
Matheus Alves <matheusalves.bmd@gmail.com>
2022-03-21 23:41:39 -0300

    Create the CloseButton component

---
+++ app/components/CloseButton/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CloseButton = () => {
+  return (
+    <button className="close-btn">
+      <i className="fas fa-times"></i>
+    </button>
+  );
+};
+
+export default CloseButton;
